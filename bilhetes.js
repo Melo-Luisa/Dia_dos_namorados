d0add1c037681cfb71fcfc9089753ef93fbe5592
Luisa_Melo <97964206+Melo-Luisa@users.noreply.github.com>
2022-06-12 10:58:40 -0300

    Add files via upload

---
+++ bilhetes.js
@@ -0,0 +1,16 @@
+var bilhetes = ['Te Amo <3', 'Meu Amor Todo <3', 'Te Adoro <3', 'Meu Nerd <3', 'Você é o dono dos meu sorrisos diários <3',
+'Você é o meu amor pá toda vida', 'CUPOM: Estou te devendo um beijo', 'Feliz dia dos namorados!', 'Meu engenheiro Favorito <3', 'DESAFIO: Tá me devendo um beijo',
+'Meu dia melhora quando estou com você <3'];
+
+var button = document.querySelector('#button'); 
+var mostra = document.querySelector('#mostra');
+
+function BilheteSorteado(){
+    var sortear = bilhetes[Math.floor(Math.random()*bilhetes.length)];
+    BilheteSorteado = sortear;
+    console.log(sortear); //mostra no console
+    //document.write(BilheteSorteado) //abre numa nova page
+    //return sortear - retorna a função e n mostra na saida
+    mostra.value = sortear //o valor é mostrado na saida
+}
+button.onclick = BilheteSorteado;
